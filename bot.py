d0c5388904cb769e96ab8552ee05807bb0d9d7f3
Nidusha <92713783+Amarasinghenidusha@users.noreply.github.com>
2022-02-24 06:14:25 -0800

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,14 @@
+from telegram.ext import Updater
+import requests
+
+BOT_Token="5236681311:AAFE8lrfxKHjnr0ZDG8mZgnFGhFdS6IDE5M"
+updater = Updater(BOT_Token)
+
+def incoming_message_action(update, context):
+  image_url=requests.get('https://api.stm-developers.software/logo?name='+(update.message.text).replace(' ','%20'))
+  context.bot.sendPhoto(chat_id=update.message.chat.id, photo=image_url,
+                          reply_to_message_id=update.message.reply_to_message.message_id)
+
+updater.dispatcher.add_handler( MessageHandler(Filters.text, incoming_message_action))
+updater.start_polling()
+updater.idle()
